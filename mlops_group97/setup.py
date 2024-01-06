a6ea734a29ae74192dd47a37ec614cd1be3a8a9a
anbipa <aniol.bisquert@gmail.com>
2024-01-06 18:30:30 +0100

    new structure

---
+++ mlops_group97/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Pistacho Classification Project',
+    author='group97',
+    license='MIT',
+)
