5f6a336ce818cc0e87939c48333a277f270d321a
Ander Barrio Campos <anderbarrio@gmail.com>
2024-01-16 10:28:56 +0100

    original model improved

--- pistachio/models/model.py
+++ pistachio/models/model.py
@@ -9,13 +9,15 @@ class MyAwesomeModel(nn.Module):
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(1080000, 128)
         self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.5)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 2)  # 2 classes for binary classification
+        self.fc3 = nn.Linear(64, 2)
 
-    def forward(self, x: Tensor):
+    def forward(self, x):
         x = self.flatten(x)
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout1(x)
         x = self.fc2(x)
         x = self.relu(x)
         x = self.fc3(x)
